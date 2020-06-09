b24b14faeb52a3d987eddf04a804acb74f96eb22
heyfuturejesse <jesse@heyfuturejesse.com>
2020-06-09 23:53:26 +1000

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Jesse Russell on 9/6/20.
+//  Copyright © 2020 Jesse Russell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
