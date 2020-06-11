af3065decb01fc0c32bf03e36b235ed04855a91b
heyfuturejesse <jesse@heyfuturejesse.com>
2020-06-11 23:42:51 +1000

    Added Firebase login and register functions! Yay git status cd /Users/jruss/Documents/git/Messenger .. ... exit

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -129,6 +130,15 @@ class LoginViewController: UIViewController {
         }
         
         // Firebase Log in
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Failed to log in user with email: \(email)")
+                return
+            }
+            
+            let user = result.user
+            print("Logged in User: \(user)")
+        })
     }
     
     func alertUserLoginError() {
--- Messenger/Controllers/Login/RegisterViewController.swift
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
     private let scrollView: UIScrollView = {
@@ -198,6 +199,16 @@ class RegisterViewController: UIViewController {
         }
         
         // Firebase Log in
+        
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Error creating user")
+                return
+            }
+            
+            let user = result.user
+            print("Created User: \(user)")
+        })
     }
     
     func alertUserLoginError() {
