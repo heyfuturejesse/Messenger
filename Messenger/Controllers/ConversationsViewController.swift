6c98177e27cdab8d02e78dd2875f1db4a8e733b9
heyfuturejesse <jesse@heyfuturejesse.com>
2020-06-14 14:45:19 +1000

    Set up Google Sign In, Handle Sign out for Google and Facebook.

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -32,7 +32,5 @@ class ConversationsViewController: UIViewController {
         }
     }
     
-    
-
 }
 
--- Messenger/Controllers/ProfileViewController.swift
+++ Messenger/Controllers/ProfileViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import FirebaseAuth
+import FBSDKLoginKit
+import GoogleSignIn
 
 class ProfileViewController: UIViewController {
     
@@ -53,6 +55,12 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
                                         guard let strongSelf = self else {
                                             return
                                         }
+                                        
+                                        // Log Out Facebook
+                                        FBSDKLoginKit.LoginManager().logOut()
+                                        
+                                        // Log Out Google
+                                        GIDSignIn.sharedInstance()?.signOut()
                                         
                                         do {
                                             try FirebaseAuth.Auth.auth().signOut()
